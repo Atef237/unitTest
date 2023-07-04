05206ffffc88a17c259ba646b56b83c031961360
atef237 <ateftaha12@gmail.com>
2023-07-04 04:07:51 +0300

    'id'            => ->product->id,

--- database/migrations/2023_07_02_092445_create_categories_table.php
+++ database/migrations/2023_07_02_092445_create_categories_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->double('price');
-//            $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade');
-            $table->unsignedBigInteger('category_id');
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
 };
